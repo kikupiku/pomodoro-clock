00d16e0ddefea70711c3b376210b1584eb18c3e2
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2019-10-18 18:56:50 +0200

    set functionality of start, stop, and pause button

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ let isWorkTime = true;
 let workTime = defaultWorkTime;
 let restTime = defaultRestTime;
 let remainingTime;
+setRemainingTime();
+
+let interval;
 
 let workLength = document.getElementById('work');
 let restLength = document.getElementById('rest');
 let mainDisplay = document.getElementById('main-display');
-let resetButtoon = document.getElementById('reset');
+let resetButton = document.getElementById('reset');
+let startButton = document.getElementById('start');
+let stopButton = document.getElementById('stop');
 
 workLength.addEventListener('input', (e) => {
   workTime = e.target.value;
@@ -22,7 +27,7 @@ restLength.addEventListener('input', e => {
   setRemainingTime();
 });
 
-resetButtoon.addEventListener('click', () => {
+resetButton.addEventListener('click', () => {
   workTime = defaultWorkTime;
   restTime = defaultRestTime;
   isWorkTime = true;
@@ -31,6 +36,47 @@ resetButtoon.addEventListener('click', () => {
   setRemainingTime();
   updateDisplayTime();
 });
+
+startButton.addEventListener('click', () => {
+  (document.querySelector('.fa-pause') !== null) ? stopClock() : startClock();
+
+  toggleStartPause();
+});
+
+stopButton.addEventListener('click', () => {
+  stopClock();
+  showPlay();
+  setRemainingTime();
+  updateDisplayTime();
+
+  //updateProgressBar;
+});
+
+function toggleStartPause() {
+  (document.querySelector('.fa-play') !== null) ? showPause() : showPlay();
+}
+
+function showPause() {
+  startButton.classList.remove('fa-play');
+  startButton.classList.add('fa-pause');
+}
+
+function showPlay() {
+  startButton.classList.remove('fa-pause');
+  startButton.classList.add('fa-play');
+}
+
+function startClock() {
+  interval = setInterval(() => {
+    console.log('one second has passed');
+    remainingTime -= 1;
+    updateDisplayTime();
+  }, 1000);
+}
+
+function stopClock() {
+  clearInterval(interval);
+}
 
 function setRemainingTime() {
   remainingTime = (isWorkTime) ? workTime * 60 : restTime * 60;
