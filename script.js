7a8b15aa34a0bad0f25d2b76f6e76d2e71a39329
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2019-10-20 14:07:34 +0200

    reorder the js file for clarity

--- script.js
+++ script.js
@@ -1,90 +1,84 @@
 const defaultWorkTime = 25;
 const defaultRestTime = 5;
-
 const workRed = 31;
-const workBlue = 117;
-const workGreen = 101;
-
+const workBlue = 101;
+const workGreen = 117;
 const restRed = 96;
-const restBlue = 155;
-const restGreen = 173;
+const restBlue = 173;
+const restGreen = 155;
 
-let decimalPercentLeft;
-let changeInRed;
-let changeInBlue;
-let changeInGreen;
+const workRestDiffRed = Math.abs(workRed - restRed);
+const workRestDiffBlue = Math.abs(workBlue - restBlue);
+const workRestDiffGreen = Math.abs(workGreen - restGreen);
 
-let workRestDiffRed = Math.abs(workRed - restRed);
-let workRestDiffBlue = Math.abs(workBlue - restBlue);
-let workRestDiffGreen = Math.abs(workGreen - restGreen);
+const timeToWorkAudio = new Audio('./assets/work-time.wav');
+const timeToRestAudio = new Audio('./assets/rest-time.wav');
+const click = new Audio('./assets/click.wav');
 
 let isWorkTime = true;
 let shouldPlayAudio = true;
 let workTime = defaultWorkTime;
 let restTime = defaultRestTime;
-let timeToWorkAudio = new Audio('./assets/work-time.wav');
-let timeToRestAudio = new Audio('./assets/rest-time.wav');
-let click = new Audio('./assets/click.wav');
-let remainingTime;
-setRemainingTime();
 
 let interval;
 let arrow;
+let decimalPercentLeft;
+let changeInRed;
+let changeInBlue;
+let changeInGreen;
+let remainingTime;
+setRemainingTime();
 
-let workLength = document.getElementById('work');
-let restLength = document.getElementById('rest');
-let mainDisplay = document.getElementById('main-display');
-let resetButton = document.getElementById('reset');
 let startButton = document.getElementById('start');
 let stopButton = document.getElementById('stop');
-let progressBar = document.getElementById('time-left');
-let buttons = document.getElementsByClassName('with-sound');
 let soundButton = document.getElementById('sound');
-let title = document.querySelector('title');
-let arrows = document.getElementsByClassName('arrow');
-let body = document.querySelector('body');
+let workLength = document.getElementById('work');
+let restLength = document.getElementById('rest');
+let resetButton = document.getElementById('reset');
+
 let workArrowUp = document.getElementById('work-arrow-up');
 let workArrowDown = document.getElementById('work-arrow-down');
 let restArrowUp = document.getElementById('rest-arrow-up');
 let restArrowDown = document.getElementById('rest-arrow-down');
-let message = document.querySelector('h1');
 
-function disableOrEnableArrows(able) {
-  for (let i = 0; i < arrows.length; i++) {
-    arrows[i].disabled = (able === 'disable') ? true : false;
-  }
-}
-
+let buttons = document.getElementsByClassName('with-sound');
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', () => {
     playAudio(click);
   });
 }
 
-function playAudio(audio) {
-  if (shouldPlayAudio) {
-    audio.play();
-  }
-}
+let mainDisplay = document.getElementById('main-display');
+let progressBar = document.getElementById('time-left');
+let title = document.querySelector('title');
+let arrows = document.getElementsByClassName('arrow');
+let body = document.querySelector('body');
+let message = document.querySelector('h1');
 
-workArrowUp.addEventListener('click', () => {
-  workLength.stepUp();
-  workLength.dispatchEvent(new Event('input'));
+startButton.addEventListener('click', () => {
+  (document.querySelector('.fa-pause') !== null) ? stopClock() : startClock();
+  workLength.disabled = true;
+  restLength.disabled = true;
+  disableOrEnableArrows('disable');
+  toggleStartPause();
 });
 
-workArrowDown.addEventListener('click', () => {
-  workLength.stepDown();
-  workLength.dispatchEvent(new Event('input'));
+stopButton.addEventListener('click', () => {
+  stopClock();
+  isWorkTime = true;
+  showPlay();
+  setRemainingTime();
+  updateDisplayTime();
+  workLength.disabled = false;
+  restLength.disabled = false;
+  disableOrEnableArrows('enable');
+  updateMessage('POMODORO');
+  resetColor();
 });
 
-restArrowUp.addEventListener('click', () => {
-  restLength.stepUp();
-  restLength.dispatchEvent(new Event('input'));
-});
-
-restArrowDown.addEventListener('click', () => {
-  restLength.stepDown();
-  restLength.dispatchEvent(new Event('input'));
+soundButton.addEventListener('click', () => {
+  shouldPlayAudio = shouldPlayAudio ? false : true;
+  toggleSoundMute();
 });
 
 workLength.addEventListener('input', (e) => {
@@ -125,48 +119,111 @@ resetButton.addEventListener('click', () => {
   resetColor();
 });
 
-startButton.addEventListener('click', () => {
-  (document.querySelector('.fa-pause') !== null) ? stopClock() : startClock();
-  workLength.disabled = true;
-  restLength.disabled = true;
-  disableOrEnableArrows('disable');
-  toggleStartPause();
+workArrowUp.addEventListener('click', () => {
+  workLength.stepUp();
+  workLength.dispatchEvent(new Event('input'));
 });
 
-stopButton.addEventListener('click', () => {
-  stopClock();
-  isWorkTime = true;
-  showPlay();
-  setRemainingTime();
-  updateDisplayTime();
-  workLength.disabled = false;
-  restLength.disabled = false;
-  disableOrEnableArrows('enable');
-  updateMessage('POMODORO');
-  resetColor();
+workArrowDown.addEventListener('click', () => {
+  workLength.stepDown();
+  workLength.dispatchEvent(new Event('input'));
 });
 
-soundButton.addEventListener('click', () => {
-  shouldPlayAudio = shouldPlayAudio ? false : true;
-  toggleSoundMute();
+restArrowUp.addEventListener('click', () => {
+  restLength.stepUp();
+  restLength.dispatchEvent(new Event('input'));
 });
+
+restArrowDown.addEventListener('click', () => {
+  restLength.stepDown();
+  restLength.dispatchEvent(new Event('input'));
+});
+
+function setRemainingTime() {
+  remainingTime = maxTime();
+}
+
+function maxTime() {
+  return (isWorkTime) ? workTime * 60 : restTime * 60;
+}
+
+function startClock() {
+  interval = setInterval(() => {
+    remainingTime -= 1;
+    updateDisplayTime();
+    switchWorkRest();
+    changeBackground();
+  }, 1000);
+}
+
+function stopClock() {
+  clearInterval(interval);
+}
+
+function updateDisplayTime() {
+  let minutes = Math.floor(remainingTime / 60);
+  let seconds = remainingTime % 60;
+
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  let time = minutes + ':' + seconds;
+  mainDisplay.textContent = time;
+  title.textContent = isWorkTime ? time + ' of work left' : time + ' of rest left';
+  updateProgressBar();
+}
+
+function updateProgressBar() {
+  progressBar.style.width = (remainingTime * 100) / maxTime() + '%';
+}
+
+function switchWorkRest() {
+  if (remainingTime <= 0) {
+    isWorkTime = isWorkTime ? false : true;
+    let audio = isWorkTime ? timeToRestAudio : timeToWorkAudio;
+    playAudio(audio);
+    setRemainingTime();
+    isWorkTime ? updateMessage('TIME TO WORK') : updateMessage('REST A LITTLE');
+  }
+}
+
+function playAudio(audio) {
+  if (shouldPlayAudio) {
+    audio.play();
+  }
+}
 
 function updateMessage(newMessage) {
   message.textContent = newMessage;
 }
 
-function toggleSoundMute() {
-  (document.querySelector('.fa-volume-up') !== null) ? showMute() : showSound();
+function changeBackground() {
+  decimalPercentLeft = remainingTime / maxTime();
+
+  changeInRed = Math.floor(decimalPercentLeft * workRestDiffRed);
+  changeInGreen = Math.floor(decimalPercentLeft * workRestDiffGreen);
+  changeInBlue = Math.floor(decimalPercentLeft * workRestDiffBlue);
+
+  isWorkTime ? transitionColors(restRed, restGreen, restBlue) : transitionColors(workRed, workGreen, workBlue);
 }
 
-function showMute() {
-  soundButton.classList.add('fa-volume-mute');
-  soundButton.classList.remove('fa-volume-up');
+function transitionColors(finishRed, finishGreen, finishBlue) {
+  startRed = (finishRed === restRed) ? workRed : restRed;
+  startGreen = (finishGreen === restGreen) ? workGreen : restGreen;
+  startBlue = (finishBlue === restBlue) ? workBlue : restBlue;
+
+  let redInTransition = (finishRed < startRed) ? finishRed + changeInRed : finishRed - changeInRed;
+  let greenInTransition = (finishGreen < startGreen) ? finishGreen + changeInGreen : finishGreen - changeInGreen;
+  let blueInTransition = (finishBlue < startBlue) ? finishBlue + changeInBlue : finishBlue - changeInBlue;
+
+  body.style.backgroundColor = `rgb(${redInTransition}, ${greenInTransition}, ${blueInTransition}`;
 }
 
-function showSound() {
-  soundButton.classList.add('fa-volume-up');
-  soundButton.classList.remove('fa-volume-mute');
+function disableOrEnableArrows(able) {
+  for (let i = 0; i < arrows.length; i++) {
+    arrows[i].disabled = (able === 'disable') ? true : false;
+  }
 }
 
 function toggleStartPause() {
@@ -185,75 +242,22 @@ function showPlay() {
   updateMessage('PAUSED POMODORO');
 }
 
-function startClock() {
-  interval = setInterval(() => {
-    remainingTime -= 1;
-    updateDisplayTime();
-    switchWorkRest();
-    changeBackground();
-  }, 1000);
-}
-
-function changeBackground() {
-  decimalPercentLeft = remainingTime / maxTime();
-
-  changeInRed = Math.floor(decimalPercentLeft * workRestDiffRed);
-  changeInBlue = Math.floor(decimalPercentLeft * workRestDiffBlue);
-  changeInGreen = Math.floor(decimalPercentLeft * workRestDiffGreen);
-
-  isWorkTime ? transitionColors(restRed, restBlue, restGreen) : transitionColors(workRed, workBlue, workGreen);
-}
-
-function transitionColors(finishRed, finishBlue, finishGreen) {
-  startRed = (finishRed === restRed) ? workRed : restRed;
-  startBlue = (finishBlue === restBlue) ? workBlue : restBlue;
-  startGreen = (finishGreen === restGreen) ? workGreen : restGreen;
-
-  let redInTransition = (finishRed < startRed) ? finishRed + changeInRed : finishRed - changeInRed;
-  let blueInTransition = (finishBlue < startBlue) ? finishBlue + changeInBlue : finishBlue - changeInBlue;
-  let greenInTransition = (finishGreen < startGreen) ? finishGreen + changeInGreen : finishGreen - changeInGreen;
-
-  body.style.backgroundColor = `rgb(${redInTransition}, ${blueInTransition}, ${greenInTransition}`;
-}
-
 function resetColor() {
-  body.style.backgroundColor = 'rgb(31, 117, 101)';
+  body.style.backgroundColor = `rgb(${workRed}, ${workGreen}, ${workBlue})`;
 }
 
-function stopClock() {
-  clearInterval(interval);
+function toggleSoundMute() {
+  (document.querySelector('.fa-volume-up') !== null) ? showMute() : showSound();
 }
 
-function switchWorkRest() {
-  if (remainingTime <= 0) {
-    isWorkTime = isWorkTime ? false : true;
-    let audio = isWorkTime ? timeToRestAudio : timeToWorkAudio;
-    playAudio(audio);
-    setRemainingTime();
-    isWorkTime ? updateMessage('TIME TO WORK') : updateMessage('REST A LITTLE');
-  }
+function showMute() {
+  soundButton.classList.add('fa-volume-mute');
+  soundButton.classList.remove('fa-volume-up');
 }
 
-function setRemainingTime() {
-  remainingTime = maxTime();
-}
-
-function maxTime() {
-  return (isWorkTime) ? workTime * 60 : restTime * 60;
-}
-
-function updateDisplayTime() {
-  let minutes = Math.floor(remainingTime / 60);
-  let seconds = remainingTime % 60;
-
-  if (seconds < 10) {
-    seconds = '0' + seconds;
-  }
-
-  let time = minutes + ':' + seconds;
-  mainDisplay.textContent = time;
-  title.textContent = isWorkTime ? time + ' of work left' : time + ' of rest left';
-  updateProgressBar();
+function showSound() {
+  soundButton.classList.add('fa-volume-up');
+  soundButton.classList.remove('fa-volume-mute');
 }
 
 function updateWorkTimeInput() {
@@ -263,9 +267,3 @@ function updateWorkTimeInput() {
 function updateRestTimeInput() {
   restLength.value = restTime;
 }
-
-function updateProgressBar() {
-  progressBar.style.width = (remainingTime * 100) / maxTime() + '%';
-}
-// TODO:
-// prevent putting in negative numbers to input
